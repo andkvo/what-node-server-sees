76c20c046bb4eafea5337d2f0be9b6c23eea1771
Andrew Kvochick <andrew@pushpress.com>
2021-05-06 23:54:42 -0400

    add more detail to logging incl URL and method

--- server.js
+++ server.js
@@ -3,13 +3,32 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/query', (req, res, next) => {
-  console.log(req.query);
-  res.json(req.query);
-})
 
-app.listen(9999, () => {
-  console.log("server listenin");
+app.use((req, res, next) => {
+  const timer = console.time('req');
+  console.log(`${req.method} ${req.url}`);
+  console.log("Query: ", req.query);
+  console.log("Body: " + JSON.stringify(req.body, null, 2));
+  console.timeEnd('req');
+  console.log('');
+  return res.json("OK");
+});
+
+// app.get('/query', (req, res, next) => {
+//   console.log(req.query);
+//   res.json(req.query);
+// });
+
+// app.post('/', (req, res, next) => {
+//   console.log(JSON.stringify(req.body, null, 2));
+//   res.json({ success: true });
+// });
+
+const port = 8899;
+
+app.listen(port, () => {
+  console.log(`server listenin on some sweet port ${port} action, baby`);
 })
